3e7bbf9074b9b57bbb9f37c622e6e3c9ec64974c
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-19 20:04:55 +0200

    minishell.h: finished structures + added functions

--- include/minishell.h
+++ include/minishell.h
@@ -6,12 +6,14 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:53:03 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/05/17 21:30:07 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/06/19 19:43:05 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+// ------------------- EXT LIBRARIES ------------------- //
 
 # include <libft.h>
 # include <stdio.h>
@@ -28,7 +30,7 @@
 # include <termios.h>
 # include <term.h>
 
-// COLOURS MACROS
+// ------------------ COLOR MACROS ------------------ //
 
 # define WHITE "\033[0m"
 # define BOLD "\033[1m"
@@ -39,23 +41,67 @@
 # define MAGENTA "\033[35m"
 # define TURQUOISE "\033[36m"
 
-// Global variable that indicates signal reception
-extern pid_t	g_pid;
+// ---------- GLOBAL VAR: Signal Reception ---------- //
+extern pid_t	g_signal;
 
-// Structures
 
-typedef struct s_parsed
+// ------------------- STRUCTURES ------------------- //
+
+/**
+ * @struct Command List structure
+ * @details
+ * char **args: Contains the command and arguments as: args[0]="ls" args[1]="-l"
+ * char *infile: If cmd uses (<), it stores the name of the input file 
+ * char *outfile: If cmd uses (> or >>), it stores the name of the output file 
+ * int	out_append: If there is an outfile. Value 1 for (>>) and 0 for (<)
+ * char *heredoc: Stores the end delimiter (string) as: cat << hola --> heredoc="hola"
+*/
+typedef struct s_cmd
 {
-	char	*input;
-}	t_parsed;
+	char			**args;
+	char			*infile;
+	char			*outfile;
+	char			*heredoc;
+	struct s_cmd	*next;
+}	t_cmd;
 
-// Main.c functions
+/**
+ * @struct Shell Structure
+ * @details
+ * char **env: Array that contains environment variables as: evn[n]="VAR=VAR_VALUE"
+ * int exit_status: Stores the exit status of the last command executed (0 = OK | !0 = KO)
+*/
+typedef struct s_shell
+{
+	char	**env;
+	int		exit_status;
+}	t_shell;
 
-char		*read_input(void);
-void		exit_program_nl(void);
+// ------------------------------------------------------ //
+//                     MAIN FUNCTIONS                     //
+// ------------------------------------------------------ //
 
-// Parser functions
+char	*read_input(void);
+void	exit_program_nl(void);
+void	signal_handler(int signal);
 
-t_parsed	*parser(char *input);
+
+// ------------------------------------------------------ //
+//                     PARSER FOLDER                      //
+// ------------------------------------------------------ //
+
+// ------------------ PARSER FUNCTIONS ------------------ //
+t_cmd	*parser(char *input); // ?
+
+// ------------------- INIT FUNCTIONS ------------------- //
+t_cmd	init_command(void); // NOT STARTED
+t_shell *init_shell(char **envp); // ?
+
+
+// ------------------- FREE FUNCTIONS ------------------- //
+void	free_array(char **array);
+void	free_commands(t_cmd *command_lst);
+void 	free_shell(t_shell *shell);
+
 
 #endif /* MINISHELL_H */
