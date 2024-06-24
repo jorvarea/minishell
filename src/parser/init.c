c337904f5b2c59a3fb6d2f055da5201338796a60
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-24 15:18:39 +0200

    Updated functions in init.c from parser/

--- src/parser/init.c
+++ src/parser/init.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:44:05 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/06/21 13:43:27 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/06/24 15:17:44 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,30 @@ char	**init_shell_env(char **envp)
 	int		len;
 
 	len = 0;
-	while (envp[len])
-		len++;
-	env = (char **)malloc(sizeof(char *) * len + 1);
-	if (!env)
-		return (NULL);
-	len = -1;
-	while (envp[++len])
-		env[len] = ft_strdup(envp[len]);
-	env[len] = NULL;
+	if (envp)
+	{
+		while (envp[len])
+			len++;
+		env = (char **)malloc(sizeof(char *) * (len + 1));
+		if (!env)
+			return (NULL);
+		len = -1;
+		while (envp[++len])
+			env[len] = ft_strdup(envp[len]);
+		env[len] = NULL;
+	}
+	else
+		env = NULL;
 	return (env);
+}
+
+
+t_cmd	*init_command(void)
+{
+	t_cmd	*tokens;
+	
+	tokens = (t_cmd *)malloc(sizeof(t_cmd));
+	return (tokens);
 }
 
 /*
@@ -44,7 +58,8 @@ t_shell *init_shell(char **envp)
 		exit_program_nl();
 	shell->env = init_shell_env(envp);
 	shell->exit_status = 0;
+	shell->tokens = NULL;
+	shell->l_env = NULL;
+	shell->files = NULL;
 	return (shell);
 }
-
-t_cmd	init_command(void);
