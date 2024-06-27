d459fcfb8196cf6594dccadaf5ca267ddba64e24
jorvarea <jorvarea@users.noreply.github.com>
2024-06-27 22:48:57 +0200

    added an equal_Str function

--- src/exec/utils/utils.c
+++ src/exec/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 12:44:12 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/06/27 14:58:20 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/06/27 22:11:44 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,14 @@ void	free_array_null(char ***array)
 		free(*array);
 		*array = NULL;
 	}
+}
+
+bool	equal_str(char *s1, char *s2)
+{
+	int	len1;
+	int	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	return ((len1 == len2) && (ft_strncmp(s1, s2, len1) == 0));
 }
