13a71b9ca28e620658b8989250bcde5e726dd7d5
jorvarea <jorvarea@users.noreply.github.com>
2024-07-01 23:55:01 +0200

    trimmed quotes before executing

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,16 +6,32 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 00:25:52 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/01 22:09:13 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/01 23:51:22 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	trim_quotes(char **args)
+{
+	char	*arg_trimmed;
+	int		i;
+
+	i = 0;
+	while (args[i])
+	{
+		arg_trimmed = ft_strtrim(args[i], "\"\'");
+		free(args[i]);
+		args[i] = arg_trimmed;
+		i++;
+	}
+}
+
 void	exec(t_shell *shell, char **args)
 {
 	shell->exit_status = 0;
 	expand_cmd(shell, args);
+	trim_quotes(args);
 	if (args && equal_str(args[0], "echo"))
 		echo(args);
 	else if (equal_str(args[0], "pwd"))
@@ -29,5 +45,5 @@ void	exec(t_shell *shell, char **args)
 	else if (equal_str(args[0], "unset"))
 		unset(shell, args);
 	else
-		executable(shell, args);
+		execute_bin(shell, args);
 }
