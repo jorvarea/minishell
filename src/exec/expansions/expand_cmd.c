8e6f3fe9e5369bc31b6696c7ce1df24a2fb0ad18
jorvarea <jorvarea@users.noreply.github.com>
2024-07-08 18:06:37 +0200

    moved the wildcard expansions somewhere else

--- src/exec/expansions/expand_cmd.c
+++ src/exec/expansions/expand_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:03:19 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/08 11:26:44 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:50:32 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,6 @@ static void	expand_arg(t_shell *shell, char **ptr_arg)
 		else if (arg[i] == '$' && (!quotes.single_quotes
 				|| (quotes.double_quotes && quotes.inner_q == SINGLE)))
 			replace_env(shell, ptr_arg, i);
-		else if (arg[i] == '*' && !quotes.single_quotes
-			&& !quotes.double_quotes)
-			replace_wildcard(shell, ptr_arg, i);
 		arg = *ptr_arg;
 		i++;
 	}
