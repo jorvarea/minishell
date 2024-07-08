a66b14cfa7404965976483d7f3e79c131e3bc8a4
jorvarea <jorvarea@users.noreply.github.com>
2024-07-08 11:31:44 +0200

    fixed how quotes are managed

--- src/exec/expansions/expand_cmd.c
+++ src/exec/expansions/expand_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:03:19 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/02 21:03:18 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/08 11:26:44 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,52 @@ typedef enum e_inner_quotes
 	NONE = 0,
 	SINGLE = 1,
 	DOUBLE = 2,
-}			t_inner_quotes;
+}					t_inner_quotes;
 
-void	manage_quotes(char quotes, bool *single_quotes, bool *double_quotes,
-		t_inner_quotes *inner_q)
+typedef struct s_quotes
 {
-	if (quotes == '\'')
+	bool			single_quotes;
+	bool			double_quotes;
+	t_inner_quotes	inner_q;
+}					t_quotes;
+
+void	manage_quotes(char char_quotes, t_quotes *quotes)
+{
+	if (char_quotes == '\'')
 	{
-		*single_quotes = !*single_quotes;
-		*inner_q = SINGLE;
+		quotes->single_quotes = !quotes->single_quotes;
+		quotes->inner_q = SINGLE;
 	}
-	else if (quotes == '\"')
+	else if (char_quotes == '\"')
 	{
-		*double_quotes = !*double_quotes;
-		*inner_q = DOUBLE;
+		quotes->double_quotes = !quotes->double_quotes;
+		quotes->inner_q = DOUBLE;
 	}
 }
 
-static void	expand_arg(t_shell *shell, char **ptr_arg, bool *single_quotes,
-		bool *double_quotes)
+static void	expand_arg(t_shell *shell, char **ptr_arg)
 {
-	t_inner_quotes	inner_q;
-	char			*arg;
-	int				i;
+	t_quotes	quotes;
+	char		*arg;
+	int			i;
 
 	arg = *ptr_arg;
-	inner_q = NONE;
+	ft_memset(&quotes, 0, sizeof(quotes));
 	i = 0;
 	while (arg[i])
 	{
 		if (arg[i] == '\'' || arg[i] == '\"')
-			manage_quotes(arg[i], single_quotes, double_quotes, &inner_q);
-		else if (arg[i] == '~' && !*single_quotes && !*double_quotes && i == 0
-			&& (arg[i + 1] == '/' || arg[i + 1] == '\0'))
+			manage_quotes(arg[i], &quotes);
+		else if (arg[i] == '~' && !quotes.single_quotes && !quotes.double_quotes
+			&& i == 0 && (arg[i + 1] == '/' || arg[i + 1] == '\0'))
 			replace_home(shell, ptr_arg, i);
-		else if (arg[i] == '$' && arg[i + 1] == '?' && !*single_quotes)
+		else if (arg[i] == '$' && arg[i + 1] == '?' && !quotes.single_quotes)
 			replace_exit_status(shell, ptr_arg, i);
-		else if (arg[i] == '$' && (!*single_quotes || 
-				(*double_quotes && inner_q == SINGLE)))
+		else if (arg[i] == '$' && (!quotes.single_quotes
+				|| (quotes.double_quotes && quotes.inner_q == SINGLE)))
 			replace_env(shell, ptr_arg, i);
-		else if (arg[i] == '*' && !*single_quotes && !*double_quotes)
+		else if (arg[i] == '*' && !quotes.single_quotes
+			&& !quotes.double_quotes)
 			replace_wildcard(shell, ptr_arg, i);
 		arg = *ptr_arg;
 		i++;
@@ -65,16 +71,12 @@ static void	expand_arg(t_shell *shell, char **ptr_arg, bool *single_quotes,
 
 void	expand_cmd(t_shell *shell, char **args)
 {
-	bool	single_quotes;
-	bool	double_quotes;
-	int		i;
+	int	i;
 
-	single_quotes = false;
-	double_quotes = false;
 	i = 0;
 	while (args[i])
 	{
-		expand_arg(shell, &args[i], &single_quotes, &double_quotes);
+		expand_arg(shell, &args[i]);
 		i++;
 	}
 }
