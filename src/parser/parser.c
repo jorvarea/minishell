da229c99f4b20beef0f1b143807e61a35a44f9ba
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-08 19:27:22 +0200

    Added almost functional split_input

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:28:08 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/06/29 22:11:24 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:27:06 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,19 @@ t_cmd	*parser(char *input, t_shell *shell)
 {
 	t_cmd	*command_lst;
 	size_t	num_cmd;
+	char	**input_array;
 
 	(void)shell;
 	if (!input)
 		return (NULL);
 	num_cmd = command_counter(input);
-	split_input(input);
+	input_array = split_input(input, ' ');
+	print_array(input_array);
+	free_array(&input_array);
+	//printf("NUM COMMANDS: %zu\n", num_cmd);
 	//printf("Input: %s\nNodes in command_lst: %zu\n", input, num_cmd);
 	command_lst = (t_cmd *)malloc(sizeof(t_cmd));
+	//print_command_lst(command_lst);
 	if (!command_lst)
 		return (NULL);
 	return (command_lst);
@@ -45,37 +50,37 @@ t_cmd	*parser(char *input, t_shell *shell)
 /*
 FUNCION BORRADOR, NO TENER EN CUENTA
 */
-char	**trim_input(char **input)
-{
-	char	*trim;
-	int		i;
+// char	**trim_input(char **input)
+// {
+// 	char	*trim;
+// 	int		i;
 
-	i = -1;
-	while (input[++i])
-	{
-		trim = ft_strtrim(input[i], " \t"); // finish chars to trim
-		sh_free_str(input[i]);
-		input[i] = trim;
-	}
-	return (input);
-}
+// 	i = -1;
+// 	while (input[++i])
+// 	{
+// 		trim = ft_strtrim(input[i], " \t"); // finish chars to trim
+// 		sh_free_str(input[i]);
+// 		input[i] = trim;
+// 	}
+// 	return (input);
+// }
 
 /*
 FUNCION BORRADOR, NO TENER EN CUENTA
 */
-char	**split_input(char *input)
-{
-	char	**input_split;
-	char	**input_trimmed;
+// char	**split_input(char *input)
+// {
+// 	char	**input_split;
+// 	char	**input_trimmed;
 
-	input_split = ft_split(input, '"');
-	input_trimmed = trim_input(input_split);
-	if (!input_split)
-		return (NULL);
-	//print_array(input_split);
-	free_array(&input_split);
-	return (NULL);
-}
+// 	input_split = ft_split(input, '"');
+// 	input_trimmed = trim_input(input_split);
+// 	if (!input_split)
+// 		return (NULL);
+// 	//print_array(input_split);
+// 	free_array(&input_split);
+// 	return (NULL);
+// }
 
 /*
 FUNCION BORRADOR, NO TENER EN CUENTA
@@ -91,5 +96,5 @@ size_t	command_counter(char *input)
 	{
 		i++;
 	}
-	return (n);
+	return (i);
 }
