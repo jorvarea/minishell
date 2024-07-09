682e711f4693deca13f5dd2abb9ed5b6afaed5c6
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-09 20:43:36 +0200

    Added input and command split to parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:28:08 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/09 13:33:20 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/09 20:37:50 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,24 @@
 t_cmd	*parser(char *input, t_shell *shell)
 {
 	t_cmd	*command_lst;
+	char	**commands;
 	char	**input_array;
+	int		i;
 
 	(void)shell;
+	i = -1;
 	if (!input)
 		return (NULL);
 	input_array = split_input(input);
+	if (!input_array)
+		return (NULL);
 	print_array(input_array);
+	while (input_array[++i])
+	{
+		commands = split_input(input_array[i]);
+		print_array(commands);
+		free_array(&commands);
+	}
 	free_array(&input_array);
 	command_lst = (t_cmd *)malloc(sizeof(t_cmd));
 	if (!command_lst)
