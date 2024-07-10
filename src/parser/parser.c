522fd385c055a530b35e6ad5fe725ed4d3663ebe
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-10 14:24:23 +0200

    Cleaned and removed unnecessary code in parser.c
    Deleted double check of !input (done in main)
    Removed old functions that are no longer needed

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,25 +6,12 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:28:08 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/09 20:37:50 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/10 14:23:54 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-/*
-	FUNCIONALIDAD: 
-	Convertir input en lista de comandos. Teniendo en cuenta: 
-		- argumentos, redirecciones y pipes
-
-	PASOS: 
-		- Crear + init command_lst y node
-		- Dividir input en tokens (tokenizar)
-		- Parsear tokens (while tokens...)
-			- Crear comandos a partir de los tokens (init? mirar redirecciones)
-			- 1st cmd
-			- manejar pipe si es necesario (crear sig comando etc ?)
-*/
 t_cmd	*parser(char *input, t_shell *shell)
 {
 	t_cmd	*command_lst;
@@ -34,8 +21,6 @@ t_cmd	*parser(char *input, t_shell *shell)
 
 	(void)shell;
 	i = -1;
-	if (!input)
-		return (NULL);
 	input_array = split_input(input);
 	if (!input_array)
 		return (NULL);
@@ -52,56 +37,3 @@ t_cmd	*parser(char *input, t_shell *shell)
 		return (NULL);
 	return (command_lst);
 }
-
-/*
-FUNCION BORRADOR, NO TENER EN CUENTA
-*/
-// char	**trim_input(char **input)
-// {
-// 	char	*trim;
-// 	int		i;
-
-// 	i = -1;
-// 	while (input[++i])
-// 	{
-// 		trim = ft_strtrim(input[i], " \t"); // finish chars to trim
-// 		sh_free_str(input[i]);
-// 		input[i] = trim;
-// 	}
-// 	return (input);
-// }
-
-/*
-FUNCION BORRADOR, NO TENER EN CUENTA
-*/
-// char	**split_input(char *input)
-// {
-// 	char	**input_split;
-// 	char	**input_trimmed;
-
-// 	input_split = ft_split(input, '"');
-// 	input_trimmed = trim_input(input_split);
-// 	if (!input_split)
-// 		return (NULL);
-// 	//print_array(input_split);
-// 	free_array(&input_split);
-// 	return (NULL);
-// }
-
-/*
-FUNCION BORRADOR, NO TENER EN CUENTA
-
-size_t	command_counter(char *input)
-{
-	int		i;
-	size_t	n;
-
-	i = 0;
-	n = 0;
-	while (input[i])
-	{
-		i++;
-	}
-	return (i);
-}
-*/
