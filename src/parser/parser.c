d6d8de88ada5c5b915d3475274a79e050259dbe0
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-10 18:43:43 +0200

    Temporary changes for testing split of input/cmd

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:28:08 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/10 15:30:04 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:37:37 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,24 @@ t_cmd	*parser(char *input, t_shell *shell)
 
 	(void)shell;
 	i = -1;
+	printf("%sINPUT>%s ", RED, WHITE);
 	input_array = split_input(input);
 	if (!input_array)
 		return (NULL);
 	print_array(input_array);
-	while (input_array[++i])
-	{
-		commands = split_cmd(input_array[i]);
-		print_array(commands);
-		free_array(&commands);
-	}
+	printf("\n%sCOMMANDS>%s ", RED, WHITE);
+	commands = split_cmd(input);
+	if (!commands)
+		return (free_array(&input_array), NULL);
+	print_array(commands);
+	// while (input_array[++i])
+	// {
+	// 	commands = split_cmd(input_array[i]);
+	// 	print_array(commands);
+	// 	free_array(&commands);
+	// }
 	free_array(&input_array);
+	free_array(&commands);
 	command_lst = (t_cmd *)malloc(sizeof(t_cmd));
 	if (!command_lst)
 		return (NULL);
