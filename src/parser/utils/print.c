e8b5f973f01253866c45931213a04abfce7f367e
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-16 12:37:58 +0200

    Updated print_array function

--- src/parser/utils/print.c
+++ src/parser/utils/print.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 14:54:05 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/10 15:51:12 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/16 12:37:51 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ void	print_shell_l_env(t_env *l_env)
 	}
 }
 
-void	print_array(char **array)
+void	print_array(char **array, char *type)
 {
 	int	i;
 
 	i = -1;
-	printf("PRINTING ARRAY:\n");
+	printf("PRINTING %s:\n", type);
 	while (array[++i])
 	{
 		printf("\t%i:%s%s%s$\n", i + 1, GREEN, array[i], WHITE);
