4ae39339fcf3adc33ad34a96298cd26f840cfb6b
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-16 17:02:51 +0200

    Updated free functions (commands && shell)

--- src/parser/utils/free.c
+++ src/parser/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 19:21:11 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/15 15:18:08 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:02:15 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,30 @@ void	free_commands(t_cmd *command_lst)
 {
 	t_cmd	*next;
 
+	next = (t_cmd *)malloc(sizeof(t_cmd));
 	while (command_lst)
 	{
 		next = command_lst->next;
+		if (next)
+			next->prev = NULL;
 		free(command_lst->cmd);
 		free_array(&command_lst->args);
 		free_redir(command_lst->redir);
 		free(command_lst);
 		command_lst = NULL;
 		command_lst = next;
-		command_lst->prev = NULL;
 	}
+	free(next);
 }
 
 void	free_shell(t_shell *shell)
 {
 	if (shell->env)
 		free_array(&shell->env);
-	if (shell->tokens)
-		free_commands(shell->tokens);
 	if (shell->l_env)
 		free_env_list(shell->l_env);
 	if (shell->files)
 		free_arg_lst(shell->files);
 	free(shell);
-	printf("%sSuccesfully freed shell%s\n", GREEN, WHITE);
+	printf("\n%sSuccesfully freed shell%s\n", GREEN, WHITE);
 }
