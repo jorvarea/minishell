c80979c4939616e1288cf1e0d06161eb7c867d44
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-07-19 20:15:46 +0200

    minor fix

--- src/exec/execute_redir.c
+++ src/exec/execute_redir.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:05:21 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/19 20:08:21 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/19 20:15:24 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ static void	restore_original_io(int original_stdin, int original_stdout)
 	close(original_stdout);
 }
 
-void	execute_redir(t_shell *shell, t_cmd *cmd, int fd_src, int fd_dest)
+void	execute_redir(t_shell *shell, t_cmd *cmd)
 {
 	t_redir	*redir;
 	int		original_stdin;
