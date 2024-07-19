9d3ea3c50e407026a7b5480f57283e0a86c8f87b
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-19 19:39:04 +0200

    Renamed file for adding new util functions
     - Renamed from exit_program_nl.c to utils.c
     - Added new function "array_len" to file

--- src/parser/utils/utils.c
+++ src/parser/utils/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit_program_nl.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 22:39:12 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/01 22:39:24 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:27:43 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,14 @@ void	exit_program_nl(void)
 {
 	printf("\n");
 	exit(EXIT_SUCCESS);
+}
+
+int	array_len(char **array)
+{
+	int	len;
+
+	len = -1;
+	while (array[++len])
+		;
+	return (len);
 }
