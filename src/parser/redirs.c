7ec9a8fb442dfaf59fd8d3e73ded767aa9909fe2
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-20 22:46:55 +0200

    FINISHED IMPLEMENTING REDIRECTIONS! :D

--- src/parser/redirs.c
+++ src/parser/redirs.c
@@ -6,13 +6,94 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 21:30:21 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/19 21:39:08 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/20 22:45:58 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	assign_redirs(t_shell *shell)
+enum e_type_redir	get_redir_type(char *type)
 {
-	(void)shell;
+	if (equal_str(type, "<"))
+		return (INFILE);
+	else if (equal_str(type, "<<"))
+		return (HEREDOC);
+	else if (equal_str(type, ">"))
+		return (OUTFILE);
+	else if (equal_str(type, ">>"))
+		return (APPEND);
+	else
+		return (NOT_REDIR);
+}
+
+bool	check_redir_args(char **redir, t_shell *shell)
+{
+	if (array_len(redir) != 2 || !redir[0] || get_redir_type(redir[0]) < 0)
+		ft_redir_error(redir[0], shell);
+	else if (!redir[1] || get_token_type(redir + 1) != CMD)
+		ft_redir_error(redir[1], shell);
+	else
+		return (0);
+	return (1);
+}
+
+t_redir	*assign_redir(t_redir *assign, char **args)
+{
+	t_redir	*new_redir;
+
+	new_redir = (t_redir *)malloc(sizeof(t_redir));
+	if (!new_redir)
+		return (NULL);
+	new_redir->fd = -1;
+	new_redir->type = get_redir_type(args[0]);
+	new_redir->file = ft_strdup(args[1]);
+	new_redir->next = NULL;
+	if (!assign)
+	{
+		new_redir->prev = NULL;
+		assign = new_redir;
+	}
+	else
+	{
+		new_redir->prev = redir_last_node(assign);
+		redir_last_node(assign)->next = new_redir;
+	}
+	return (assign);
+}
+
+void	update_redir(t_cmd *redir, t_shell *shell)
+{
+	t_cmd	*prev;
+	t_cmd	*next;
+	t_cmd	*assign;
+
+	if (check_redir_args(redir->args, shell))
+		return ;
+	prev = redir->prev;
+	next = redir->next;
+	while (next && next->type == REDIR)
+		next = next->next;
+	while (prev && prev->type == REDIR)
+		prev = prev->prev;
+	if (prev && (prev->type == CMD || prev->type == CLOSE_PAR))
+		assign = prev;
+	else if (next && next->type == CMD)
+		assign = next;
+	else
+		assign = redir;
+	assign->redir = assign_redir(assign->redir, redir->args);
+	update_redir_token(redir);
+}
+
+void	get_redirs(t_shell *shell)
+{
+	t_cmd	*node;
+
+	node = shell->tokens;
+	while (node)
+	{
+		if (node->type == REDIR)
+			update_redir(node, shell);
+		node = node->next;
+	}
 }
