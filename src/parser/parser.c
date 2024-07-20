7045088fa5f0b45a86e9a1f95e92580edbb8f241
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-20 22:47:59 +0200

    Removed lines used for testing parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:28:08 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/19 21:44:29 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/20 22:47:37 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ t_cmd	*parser(char *input, t_shell *shell)
 {
 	char	*p_input;
 	char	**input_array;
-	char	*org_input;
 
-	org_input = input;
 	shell->tokens = NULL;
 	input = process_input(input, shell);
 	if (!input)
@@ -26,11 +24,9 @@ t_cmd	*parser(char *input, t_shell *shell)
 	p_input = ft_strtrim(input, " \t\n\v\f\r");
 	if (!p_input)
 		return (shell->tokens);
-	printf("[%i>%i]:|%s|\n", ft_strlen(org_input), ft_strlen(p_input), p_input);
 	input_array = split_parser(p_input);
 	if (!input_array)
 		return (free(p_input), shell->tokens);
-	print_array_one_line(input_array, "Split Parser");
 	new_token_list(input_array, shell);
 	free_array(&input_array);
 	return (free(p_input), shell->tokens);
