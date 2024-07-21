67606e18af5068b32db104f8f4b46064f707b18d
jorvarea <jorvarea@users.noreply.github.com>
2024-07-21 14:40:39 +0200

    pipe works but can't continue after that

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:38:27 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/20 00:52:18 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/21 14:38:46 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,36 @@
 
 static void	exec_one(t_shell *shell, t_cmd *cmd)
 {
+	execute_redir(shell, cmd);
+	exit(shell->exit_status);
+}
+
+static void	exec_last(t_shell *shell, t_cmd *cmd, int original_stdout)
+{
 	pid_t	pid;
 	int		status;
 
+	safe_dup2(original_stdout, STDOUT_FILENO);
 	pid = safe_fork();
 	if (pid == 0)
-		execute_redir(shell, cmd);
-	wait(&status);
-	if (WIFEXITED(status))
-		shell->exit_status = WEXITSTATUS(status);
+		exec_one(shell, cmd);
 	else
-		shell->exit_status = 130;
+	{
+		close(original_stdout);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			shell->exit_status = WEXITSTATUS(status);
+		else
+			shell->exit_status = 128 + WTERMSIG(status);
+	}
+}
+
+static void exec_left(t_shell *shell, t_cmd *cmd, int pipe_fds[2])
+{
+	close(pipe_fds[0]);
+	safe_dup2(pipe_fds[1], STDOUT_FILENO);
+	close(pipe_fds[1]);
+	exec_one(shell, cmd->prev);
 }
 
 void	exec(t_shell *shell, t_cmd *cmd, int original_stdout)
@@ -34,26 +53,20 @@ void	exec(t_shell *shell, t_cmd *cmd, int original_stdout)
 
 	if (!cmd->next)
 	{
-		safe_dup2(original_stdout, STDOUT_FILENO);
-		exec_one(shell, cmd);
-		close(original_stdout);
+		exec_last(shell, cmd, original_stdout);
 		return ;
 	}
-	else
-		cmd = cmd->next;
+	cmd = cmd->next;
 	safe_pipe(pipe_fds);
 	pid = safe_fork();
 	if (pid == 0)
+		exec_left(shell, cmd, pipe_fds);
+	else
 	{
-		safe_dup2(pipe_fds[1], STDOUT_FILENO);
-		if (cmd->prev->prev && cmd->prev->prev->type == PIPE)
-			safe_dup2(pipe_fds[0], STDIN_FILENO);
-		close(pipe_fds[0]);
 		close(pipe_fds[1]);
-		exec_one(shell, cmd->prev);
+		safe_dup2(pipe_fds[0], STDIN_FILENO);
+		close(pipe_fds[0]);
 		exec(shell, cmd->next, original_stdout);
+		waitpid(pid, NULL, 0);
 	}
-	close(pipe_fds[0]);
-	close(pipe_fds[1]);
-	wait(NULL);
 }
