aad12c61433b0287e0d7ca130809f84ab407f3f6
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-07-22 13:52:09 +0200

    duplication of original_stdin and out is out of there

--- src/exec/execute_redir.c
+++ src/exec/execute_redir.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:05:21 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/19 20:15:24 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:09:39 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,23 +49,19 @@ static void	change_std_io(t_redir *redir)
 		dup2(redir->fd, STDOUT_FILENO);
 }
 
-static void	restore_original_io(int original_stdin, int original_stdout)
+static void	restore_prev_io(int prev_stdin, int prev_stdout)
 {
-	dup2(original_stdin, STDIN_FILENO);
-	dup2(original_stdout, STDOUT_FILENO);
-	close(original_stdin);
-	close(original_stdout);
+	dup2(prev_stdin, STDIN_FILENO);
+	dup2(prev_stdout, STDOUT_FILENO);
+	close(prev_stdin);
+	close(prev_stdout);
 }
 
-void	execute_redir(t_shell *shell, t_cmd *cmd)
+void	execute_redir(t_shell *shell, t_cmd *cmd, int prev_stdin, int prev_stdout)
 {
 	t_redir	*redir;
-	int		original_stdin;
-	int		original_stdout;
 	bool	error;
 
-	original_stdin = safe_dup(STDIN_FILENO);
-	original_stdout = safe_dup(STDOUT_FILENO);
 	redir = cmd->redir;
 	error = false;
 	save_heredocs(shell, redir);
@@ -79,7 +75,7 @@ void	execute_redir(t_shell *shell, t_cmd *cmd)
 	}
 	if (!error)
 		execute_cmd(shell, cmd);
-	restore_original_io(original_stdin, original_stdout);
+	restore_prev_io(prev_stdin, prev_stdout);
 	close_files(cmd->redir);
 	remove_tmp_heredoc_files(cmd->redir);
 }
