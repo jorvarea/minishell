2025550c573f2844c8f139405a2f08b63fe880e2
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-07-22 23:52:33 +0200

    victor added some stuff there

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:38:27 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/22 21:11:26 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/22 23:41:50 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 void	exec(t_shell *shell)
 {
 	t_cmd	*cmd;
+	int		bk_in;
+	int		bk_out;
 
+	bk_in = dup(STDIN_FILENO);
+	bk_out = dup(STDOUT_FILENO);
 	cmd = shell->tokens;
 	init_fds_pid(cmd);
 	assign_pipes(cmd);
@@ -25,5 +29,9 @@ void	exec(t_shell *shell)
 			exec_one(shell, cmd);
 		cmd = cmd->next;
 	}
+	dup2(bk_in, STDIN_FILENO);
+	dup2(bk_out, STDOUT_FILENO);
+	close(bk_in);
+	close(bk_out);
 	wait_pids(shell, shell->tokens);
 }
