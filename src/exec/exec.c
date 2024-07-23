671fd316ae9a57b3363746ba3b84eef6da9ba5c0
jorvarea <jorvarea@users.noreply.github.com>
2024-07-23 17:54:35 +0200

    fix signal handling for processes

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:38:27 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/23 16:15:00 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/23 17:48:11 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	exec(t_shell *shell)
 
 	cmd = shell->tokens;
 	init_fds_pid(cmd);
-	init_signal_handler_exec();
 	while (cmd)
 	{
 		if (cmd->type == CMD)
@@ -28,5 +27,4 @@ void	exec(t_shell *shell)
 	wait_pids(shell, shell->tokens);
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
-	init_signal_handler_cli();
 }
