aae2a024ab2765801681600202be033004aee2cf
jorvarea <jorvarea@users.noreply.github.com>
2024-07-23 18:32:18 +0200

    fix single command built in

--- src/exec/exec_single_cmd.c
+++ src/exec/exec_single_cmd.c
@@ -6,32 +6,47 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 15:14:47 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/23 17:46:46 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/23 18:29:00 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	is_built_in(t_cmd *cmd)
+{
+	char	*cmd_str;
+
+	cmd_str = cmd->args[0];
+	return (equal_str(cmd_str, "echo") || equal_str(cmd_str, "cd")
+		|| equal_str(cmd_str, "env") || equal_str(cmd_str, "pwd")
+		|| equal_str(cmd_str, "export") || equal_str(cmd_str, "unset"));
+}
 
 void	exec_single_cmd(t_shell *shell)
 {
 	pid_t	pid;
 	int		status;
 
-	pid = safe_fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
+	if (is_built_in(shell->tokens))
 		execute_redir(shell, shell->tokens);
-		exit(shell->exit_status);
-	}
 	else
 	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			shell->exit_status = WEXITSTATUS(status);
+		pid = safe_fork();
+		if (pid == 0)
+		{
+			signal(SIGINT, SIG_DFL);
+			execute_redir(shell, shell->tokens);
+			exit(shell->exit_status);
+		}
 		else
-			shell->exit_status = 130;
-		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
+		{
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+				shell->exit_status = WEXITSTATUS(status);
+			else
+				shell->exit_status = 130;
+			close(STDIN_FILENO);
+			close(STDOUT_FILENO);
+		}
 	}
 }
