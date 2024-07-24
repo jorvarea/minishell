bd6a49d7a96b2ddca66eeaf2d586be0b553ed1e3
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-24 21:18:58 +0200

    Reorganized free helper functions

--- src/parser/utils/free_utils.c
+++ src/parser/utils/free_utils.c
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_utils.c                                       :+:      :+:    :+:   */
+/*   free_structs.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/24 14:05:01 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/21 15:55:47 by ana-cast         ###   ########.fr       */
+/*   Created: 2024/06/19 19:21:11 by ana-cast          #+#    #+#             */
+/*   Updated: 2024/07/24 21:16:20 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-void	sh_free_str(char *str)
-{
-	if (str)
-	{
-		free(str);
-		str = NULL;
-	}
-}
 
 void	free_array(char ***array)
 {
@@ -33,4 +24,72 @@ void	free_array(char ***array)
 		free(*array);
 		*array = NULL;
 	}
+}
+
+void	free_redir(t_redir	**redir)
+{
+	t_redir	*next;
+
+	while (*redir)
+	{
+		next = (*redir)->next;
+		if (next)
+			next->prev = NULL;
+		if ((*redir)->file)
+		{
+			free((*redir)->file);
+			(*redir)->file = NULL;
+		}
+		if ((*redir)->fd > 0)
+			close((*redir)->fd);
+		(*redir)->fd = 0;
+		free(*redir);
+		*redir = NULL;
+		*redir = next;
+	}
+}
+
+void	free_env_list(t_env	**l_env)
+{
+	t_env	*next;
+
+	while ((*l_env))
+	{
+		next = (*l_env)->next;
+		if (next)
+			next->prev = NULL;
+		free((*l_env)->key);
+		free((*l_env)->value);
+		free((*l_env));
+		(*l_env) = NULL;
+		(*l_env) = next;
+	}
+}
+
+void	*free_tokens(t_cmd **tokens)
+{
+	t_cmd	*next;
+
+	while (*tokens)
+	{
+		next = (*tokens)->next;
+		if (next)
+			next->prev = NULL;
+		free_array(&(*tokens)->args);
+		free_redir(&(*tokens)->redir);
+		free(*tokens);
+		*tokens = NULL;
+		*tokens = next;
+	}
+	return (NULL);
+}
+
+void	*free_shell(t_shell **shell)
+{
+	if ((*shell)->env)
+		free_array(&(*shell)->env);
+	if ((*shell)->l_env)
+		free_env_list(&(*shell)->l_env);
+	free(*shell);
+	return (NULL);
 }
