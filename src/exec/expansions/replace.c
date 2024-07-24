eaf5bf86d49f339b0a2733d2e9ac20c9f08ee6e4
jorvarea <jorvarea@users.noreply.github.com>
2024-07-24 13:39:50 +0200

    fix the case echo hola

--- src/exec/expansions/replace.c
+++ src/exec/expansions/replace.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 12:30:35 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/24 13:27:00 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/24 13:39:04 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ void	replace_env(t_shell *shell, char **arg, int start_index)
 		free(value);
 	}
 	else
-		*arg = ft_strdup("");
+		*arg = ft_strrep(*arg, "", start_index, end_index);
 }
 
 void	replace_home(t_shell *shell, char **arg, int start_index)
