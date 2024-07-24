a89f237b87f6c5c8dd10c7215d09e9ef49e65d5c
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-24 21:21:17 +0200

    Reorganized function prototypes from parser

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:53:03 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/24 20:49:46 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/24 21:17:48 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -243,15 +243,14 @@ int		get_redir_type(char *type);
 		// ------------------ ERROR.C ------------------- //
 void	*parser_error(int error, char *str, int exit_code, t_shell *shell);
 bool	check_redir_args(char **redir, t_shell *shell);
-int		check_error_tokens(t_shell *shell);
+int		check_token_err(t_shell *shell);
 
-		// ----------- FREE.C && FREE_UTILS.C ----------- //
+		// ---------------- FREE_UTILS.C ---------------- //
 void	*free_shell(t_shell **shell);
 void	*free_tokens(t_cmd **command_lst);
 void	free_array(char ***array);
 void	free_env_list(t_env	**l_env);
 void	free_redir(t_redir	**redir);
-void	sh_free_str(char *str);	// UNUSED
 
 		// --------------- STRUCT_NODE.C ---------------- //
 void	pop_node_from_list(t_cmd *node);
