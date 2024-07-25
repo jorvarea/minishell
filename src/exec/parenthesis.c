090baf6961127e5436c6f5a4dbf0b3d255a95a33
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-07-25 16:00:48 +0200

    parenthesis work

--- src/exec/parenthesis.c
+++ src/exec/parenthesis.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 00:31:23 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/07/25 01:29:20 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/07/25 15:56:49 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,54 @@ static void	peek_pipes_parenthesis(t_cmd *end)
 {
 	int	pipes_fd[2];
 
-	if (end->next->next && end->next->next->type == PIPE)
+	if (end->next && end->next->type == PIPE)
 	{
 		safe_pipe(pipes_fd);
 		end->outfd = pipes_fd[1];
-		end->next->next->next->infd = pipes_fd[0];
+		end->next->next->infd = pipes_fd[0];
 	}
 }
 
-static void	manage_pipes_parenthesis(t_cmd *head, t_cmd *end)
+static void	manage_redir_pipes_parenthesis(t_cmd *head, t_cmd *end)
 {
-	if (head->infd != -1)
+	if (head->prev->infd != -1)
 	{
-		dup2(head->infd, STDIN_FILENO);
-		close(head->infd);
+		dup2(head->prev->infd, STDIN_FILENO);
+		close(head->prev->infd);
 	}
 	if (end->outfd != -1)
 	{
 		dup2(end->outfd, STDOUT_FILENO);
 		close(end->outfd);
 	}
-	if (end->next->next && end->next->next->type == PIPE)
-		close(end->next->next->next->infd);
+	if (end->next && end->next->type == PIPE)
+		close(end->next->next->infd);
 }
 
 static void	execute_parenthesis(t_shell *shell, t_cmd *head, t_cmd *end)
 {
+	pid_t	pid;
+	int		status;
+
 	peek_pipes_parenthesis(end);
-	manage_pipes_parenthesis(head, end);
-	exec(shell, head, end);
+	pid = safe_fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		manage_redir_pipes_parenthesis(head, end);
+		exec(shell, head, end);
+		exit_shell_child(shell);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		update_exit_status_process(shell, status);
+		if (end->outfd >= 0)
+			close(end->outfd);
+		if (head->prev->infd >= 0)
+			close(head->prev->infd);
+	}
 }
 
 void	manage_parenthesis(t_shell *shell, t_cmd **cmd)
@@ -55,6 +74,6 @@ void	manage_parenthesis(t_shell *shell, t_cmd **cmd)
 	head = *cmd;
 	while ((*cmd)->type != CLOSE_PAR)
 		*cmd = (*cmd)->next;
-	end_node = (*cmd)->prev;
+	end_node = *cmd;
 	execute_parenthesis(shell, head, end_node);
 }
